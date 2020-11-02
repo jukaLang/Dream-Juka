1f4950030c20f21f3d8fd024f23571d9801ea535
Don <doreamey@microsoft.com>
2020-11-02 15:39:06 -0800

    old stuff

---
+++ src/DreamCompiler/Lexer/IStatement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DReAMCompiler.Lexer
+{
+    public enum StatementType
+    {
+        IfStatement,
+        WhileStatment,
+        CallStatement,
+        DeclStatment,
+        ReturnStatement,
+    }
+
+    public interface IStatement
+    {
+        StatementType Kind { get; set; }
+    }
+
+    public abstract class Statement : IStatement
+    {
+        public StatementType Kind { get; set; }
+    }
+
+    public class IfStatement : Statement
+    {
+
+    }
+
+
+
+
+}
